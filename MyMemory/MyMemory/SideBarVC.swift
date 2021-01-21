65898f3599b1941278e65bdb3f0b29e373a60f43
youngwoo <lyw2100@naver.com>
2021-01-21 15:29:36 +0900

    2021/01/21 sideBar and profile_page complete

---
+++ MyMemory/MyMemory/SideBarVC.swift
@@ -0,0 +1,88 @@
+import UIKit
+
+class SideBarVC: UITableViewController{
+    
+    var nameLabel: UILabel!
+    var emailLabel: UILabel!
+    var profileImage: UIImageView!
+    
+    let titles = ["새글 작성하기", "친구 새글", "달력으로 보기", "공지사항", "통계", "계정 관리"]
+    
+    let icons = [
+        UIImage(named: "icon01.png"),
+        UIImage(named: "icon02.png"),
+        UIImage(named: "icon03.png"),
+        UIImage(named: "icon04.png"),
+        UIImage(named: "icon05.png"),
+        UIImage(named: "icon06.png")
+    ]
+    
+    override func viewDidLoad() {
+        let headerView = UIView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: 70))
+        
+        headerView.backgroundColor = .brown
+        
+        self.tableView.tableHeaderView = headerView
+        
+        nameLabel = UILabel()
+        emailLabel = UILabel()
+        profileImage = UIImageView()
+        
+        self.nameLabel.frame = CGRect(x: 70, y: 15, width: 100, height: 30)
+        self.nameLabel.text = "꼼곰한 재은씨"
+        self.nameLabel.textColor = .white
+        self.nameLabel.font = UIFont.boldSystemFont(ofSize: 15)
+        
+        self.emailLabel.frame = CGRect(x: 70, y: 30, width: 100, height: 30)
+        self.emailLabel.text = "sqlpro@naver.com"
+        self.emailLabel.textColor = .white
+        self.emailLabel.font = UIFont.systemFont(ofSize: 11)
+        self.emailLabel.backgroundColor = .clear
+        
+        headerView.addSubview(self.emailLabel)//헤더 뷰에 추가
+        headerView.addSubview(self.nameLabel)
+        
+        let defaultProfile = UIImage(named: "account.jpg")
+        self.profileImage.image = defaultProfile
+        self.profileImage.frame = CGRect(x: 10, y: 10, width: 50, height: 50)
+        
+        self.profileImage.layer.cornerRadius = (self.profileImage.frame.width / 2)
+        self.profileImage.layer.borderWidth = 0
+        self.profileImage.layer.masksToBounds = true // 마스크 효과
+        view.addSubview(self.profileImage)//헤더 뷰에 추가
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let id = "menucell"
+        let cell = tableView.dequeueReusableCell(withIdentifier: id) ?? UITableViewCell(style: .default, reuseIdentifier: id)
+        
+        cell.textLabel?.text = self.titles[indexPath.row]
+        cell.imageView?.image = self.icons[indexPath.row]
+        
+        cell.textLabel?.font = UIFont.systemFont(ofSize: 14)
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.titles.count
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.row == 0{
+            //선택된 행이 새글 작성 메뉴일 때
+            let uv = self.storyboard?.instantiateViewController(withIdentifier: "MemoForm")
+            let target = self.revealViewController()?.frontViewController as! UINavigationController
+            
+            target.pushViewController(uv!, animated: true)
+            self.revealViewController()?.revealToggle(self)
+        } else if indexPath.row == 5{
+            let uv = self.storyboard!.instantiateViewController(withIdentifier: "_Profile")
+            uv.modalPresentationStyle = .fullScreen
+            self.present(uv, animated: true, completion: {
+                self.revealViewController().revealToggle(self)
+            })
+        }
+    }
+    
+    
+}
